db29aff97636026e2ea964744dc0d6198bb94d35
jayKrakerz <jimkeyz2015@gmail.com>
2024-12-05 12:23:03 +0000

    wip 3

--- src/components/HeatmapScreenRecording.vue
+++ src/components/HeatmapScreenRecording.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
  <main class="text-white">Hello</main>
 </template>
 <script lang="ts">
@@ -12,4 +12,54 @@ import { ref, computed, onBeforeUnmount, onMounted, Ref, defineEmits, reactive }
 
 <style scoped>
 
-</style>
+</style> -->
+
+
+
+
+
+
+<template>
+  <div class="border-t border-gray50">
+    <div class="flex">
+      <SideBar />
+      <div class="w-full">
+        <!-- Pass recordingsCount and selectedRecording as props to AppNav -->
+        <AppNav :recordingsCount="recordingsCount" :selectedRecording="selectedRecording"
+          @update:selectedRecording="selectedRecording = $event" @unselectRecording="selectedRecording = null"
+          @selectPreviousRecording="selectPreviousRecording" @selectNextRecording="selectNextRecording" />
+
+        <!-- Listen for updateRecordingsCount and selectRecording events from RecordingsTable -->
+        <RecordingsTable :selectedRecording="selectedRecording" @updateRecordingsCount="updateRecordingsCount"
+          @selectRecording="setSelectedRecording" />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import RecordingsTable from "./RecordingsTable.vue";
+import SideBar from "./SideBar.vue";
+import AppNav from "./AppNav.vue";
+import records, { Record } from '../lib/recordingsData';
+import { useRecordings } from '../composable/useRecordings';
+
+const {
+  filteredSortedRecords,
+  selectedRecording,
+  selectPreviousRecording,
+  selectNextRecording,
+} = useRecordings(records);
+
+const recordingsCount = ref(filteredSortedRecords.value.length);
+
+function updateRecordingsCount(count: number) {
+  recordingsCount.value = count;
+}
+
+function setSelectedRecording(record: Record) {
+  selectedRecording.value = record;
+}
+</script>
