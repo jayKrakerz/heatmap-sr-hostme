09f16ac8a85a99461ac76c2b612070056cc3cc51
jayKrakerz <jimkeyz2015@gmail.com>
2024-12-05 12:15:09 +0000

    chore: build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 export declare const HeatmapScreenRecording: import("vue").DefineComponent<{}, () => import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
     [key: string]: any;
-}>, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}>;
+}>, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}>;
 export default HeatmapScreenRecording;
 export type { PropType as HeatmapScreenRecordingProps } from "vue";
